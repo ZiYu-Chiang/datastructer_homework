4b1bdbe5bea183266b46d17157c3ac01da1ec7d1
ZiYu-Chiang <59796907+ZiYu-Chiang@users.noreply.github.com>
2020-01-12 22:52:07 +0800

    Update C_ST45-易 名字反了.c

--- github/字串I/C_ST45-易 名字反了.c
+++ github/字串I/C_ST45-易 名字反了.c
@@ -11,10 +11,10 @@ int main(){
 	for(i = 0;i<a;i++) {
 	 scanf("%s",line);
 	 len = strlen(line);
-	 for(j = len-1;j>=0;j--) {
-		 if(j == len-1) {
+	 for(j = len-1;j>=0;j--) {//倒著輸出
+		 if(j == len-1) { //頭的地方換大寫
 			 printf("%c",line[j]-32);
-		 }else if(j == 0){
+		 }else if(j == 0){//尾巴大寫換小寫
 			 printf("%c",line[j]+32);
 		 }else{
 			 printf("%c",line[j]);
